c55d716be27cb5e0ddd41fa2b27e5510597be5f5
joaovgatti <joaovsgt@dcc.ufrj.br>
2021-01-21 18:43:45 -0300

    main

---
+++ main.go
@@ -0,0 +1,21 @@
+package go_server
+
+import (
+	"log"
+	"net/http"
+)
+
+type InMemoryPlayerStore struct {}
+
+func(i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
+func main(){
+	server := &PlayServer{&InMemoryPlayerStore{}}
+
+
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v",err)
+	}
+}
